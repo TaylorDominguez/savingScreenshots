c4da93a47c1dd39c09770faecaa2b77f4abb77c9
MacMini18 <mcm-mac-18@MacMini18s-Mac-mini.local>
2023-05-02 14:15:39 -0700

    revised screenshot path

--- test/specs/dynamic_loading.spec.js
+++ test/specs/dynamic_loading.spec.js
@@ -4,12 +4,12 @@ describe('dynamic_loading application', () => {
     it('should click the button', async () => {
         await dynamicPage.open()
         
-        await browser.saveScreenshot('../screenshots/dynamicScreenshot1.png')
+        await browser.saveScreenshot('test/screenshots/openScreenshot.png')
         await dynamicPage.startButton.click()
         await dynamicPage.helloWorld.waitForExist()
         await expect (dynamicPage.helloWorld).toHaveTextContaining(
             'Hello World!'
         )
-        await browser.saveScreenshot('../screenshots/dynamicScreenshot2.png')
+        await browser.saveScreenshot('test/screenshots/startScreenshot2.png')
     })
 })
